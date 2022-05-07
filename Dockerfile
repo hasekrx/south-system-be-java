62c4e0954cc34ac2e6b2d0732cc0ae087e10aff0
acarri <alexandre.carriconde@ext.mercedes-benz.io>
2022-05-07 17:00:43 +0100

    removing docker integration

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM openjdk:11-slim
-
-ARG JAR_FILE=target/*.jar
-
-COPY ${JAR_FILE} app.jar
-
-EXPOSE 8080
--- src/main/java/com/southsystem/dataanalysis/DataAnalysisApplication.java
+++ src/main/java/com/southsystem/dataanalysis/DataAnalysisApplication.java
@@ -4,9 +4,13 @@ import com.southsystem.dataanalysis.config.AppConfiguration;
 import com.southsystem.dataanalysis.core.model.FileData;
 import com.southsystem.dataanalysis.service.DoneFileService;
 import com.southsystem.dataanalysis.service.EntryFileService;
+import com.southsystem.dataanalysis.service.ReadFileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -17,7 +21,23 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DataAnalysisApplication {
+
+
+	@Autowired
+	private ReadFileService readFileService;
+
+	@Autowired
+	private AppConfiguration appConfiguration;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DataAnalysisApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() throws IOException {
+		System.out.println("hello world, I have just started up");
+
+		String content = readFileService.readFile(Paths.get("/" + appConfiguration.getHomePath() + "/data/in/sample.dat"));
+		System.out.println(content);
 	}
 }
